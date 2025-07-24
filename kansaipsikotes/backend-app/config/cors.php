330ff7c906a6a6e23d37f168eef3e3ffd91a0026
himuraaaaa <xzanovpro14@gmail.com>
2025-07-24 13:11:50 +0700

    Update cors.php

--- kansaipsikotes/backend-app/config/cors.php
+++ kansaipsikotes/backend-app/config/cors.php
@@ -3,7 +3,7 @@
 return [
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['https://nextjslaravel-amg8-3dzvjrard-himuraaaaas-projects.vercel.app'],
+    'allowed_origins' => ['https://nextjslaravel-amg8.vercel.app'],
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => ['*'],
