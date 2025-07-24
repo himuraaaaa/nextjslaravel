dcb9aff73cfcaf1155e86c313e5c6cfabd31ae99
himuraaaaa <xzanovpro14@gmail.com>
2025-07-24 10:46:45 +0700

    cek

--- kansaipsikotes/frontend/utils/api.js
+++ kansaipsikotes/frontend/utils/api.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:8000/api',
+    baseURL: process.env.NEXT_PUBLIC_API_BASE
+        ? process.env.NEXT_PUBLIC_API_BASE + '/api'
+        : 'http://localhost:8000/api',
     headers: {
         'Content-Type': 'application/json',
         'Accept': 'application/json',
