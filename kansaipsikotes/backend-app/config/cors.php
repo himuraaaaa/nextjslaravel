808074f97764f67f91544774ab6a42d66b1e3c7b
himuraaaaa <xzanovpro14@gmail.com>
2025-07-24 15:50:22 +0700

    Revert "Update cors.php"
    This reverts commit 5b0b889e3768e83143b70f018a090114a9979f32.

--- kansaipsikotes/backend-app/config/cors.php
+++ kansaipsikotes/backend-app/config/cors.php
@@ -3,7 +3,7 @@
 return [
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['https://nextjslaravel-ampg8.vercel.app'],
+    'allowed_origins' => ['http://localhost:3000'],
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => ['*'],
