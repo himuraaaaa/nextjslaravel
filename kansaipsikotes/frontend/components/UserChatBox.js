e1ccfb609d565bd8e767e3d3826d72a5fc247660
himuraaaaa <xzanovpro14@gmail.com>
2025-07-24 13:32:19 +0700

    ui

--- kansaipsikotes/frontend/components/UserChatBox.js
+++ kansaipsikotes/frontend/components/UserChatBox.js
@@ -1,8 +1,8 @@
 import React, { useEffect, useRef, useState } from 'react';
 import io from 'socket.io-client';
 
-const SIGNALING_SERVER_URL = 'http://localhost:3001';
-const API_BASE = 'http://localhost:8000/api';
+//const SIGNALING_SERVER_URL = 'http://localhost:3001';
+//const API_BASE = 'http://localhost:8000/api';
 
 // Nonaktifkan fitur chat user
 const UserChatBox = () => null;
